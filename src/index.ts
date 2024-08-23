cd33b74cd2f8aabd172209f57e1809136afeaacf
Mattskywalker <mateusejesus@hotmail.com.br>
2024-08-23 15:17:46 -0300

    config: Cors configuration

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,19 @@ import 'dotenv/config';
 import express from 'express';
 import { errorMiddleware } from 'middlewares/errorMiddleware';
 import demoRoutes from 'routes/demoRoutes';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/', demoRoutes);
